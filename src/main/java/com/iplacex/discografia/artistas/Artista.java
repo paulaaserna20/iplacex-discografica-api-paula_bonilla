ade3f7eca2e86240172ff8c85f187bf04e7ff2f2
paulaaserna20 <paulaaserna20@gmail.com>
2026-09-21 18:19:35 -0300

    Corrige nombres de colecciones MongoDB

--- src/main/java/com/iplacex/discografia/artistas/Artista.java
+++ src/main/java/com/iplacex/discografia/artistas/Artista.java
@@ -6,7 +6,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-@Document("artistas")
+@Document("Artistas")
 public class Artista {
 
     @Id
--- src/main/java/com/iplacex/discografia/discos/Disco.java
+++ src/main/java/com/iplacex/discografia/discos/Disco.java
@@ -6,7 +6,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-@Document("discos")
+@Document("Discotecas")
 public class Disco {
 
     @Id
